571d9beab7997c9aa554100a1b7b9db15021b904
Manuel <manuelfernandobedoya@gmail.com>
2020-08-20 18:57:23 -0500

    Añado condiciones a la calculadora

--- behavor.js
+++ behavor.js
@@ -4,26 +4,51 @@ let secondNumber = 0;
 function sumar() {
   firstNumber = document.getElementById("number1").value;
   secondNumber = document.getElementById("number2").value;
+  if (verificarNumerosVacios()) {
+    return;
+  }
   document.getElementById("resultado").innerHTML =
-    parseInt(firstNumber) + parseInt(secondNumber);
+    parseFloat(firstNumber) + parseFloat(secondNumber);
 }
 
 function restar() {
   firstNumber = document.getElementById("number1").value;
   secondNumber = document.getElementById("number2").value;
+  if (verificarNumerosVacios()) {
+    return;
+  }
   document.getElementById("resultado").innerHTML =
-    parseInt(firstNumber) - parseInt(secondNumber);
+    parseFloat(firstNumber) - parseFloat(secondNumber);
 }
 function multiplicar() {
   firstNumber = document.getElementById("number1").value;
   secondNumber = document.getElementById("number2").value;
+  if (verificarNumerosVacios()) {
+    return;
+  }
   document.getElementById("resultado").innerHTML =
-    parseInt(firstNumber) * parseInt(secondNumber);
+    parseFloat(firstNumber) * parseFloat(secondNumber);
 }
 
 function dividir() {
   firstNumber = document.getElementById("number1").value;
   secondNumber = document.getElementById("number2").value;
+  if (verificarNumerosVacios()) {
+    return;
+  }
+  if (secondNumber == 0) {
+    alert("No se puede dividir entre 0");
+    return;
+  }
   document.getElementById("resultado").innerHTML =
-    parseInt(firstNumber) / parseInt(secondNumber);
+    parseFloat(firstNumber) / parseFloat(secondNumber);
+}
+
+function verificarNumerosVacios() {
+  if (firstNumber == "" || secondNumber == "") {
+    alert("Debe ingresar 2 numeros");
+    return true;
+  } else {
+    return false;
+  }
 }
