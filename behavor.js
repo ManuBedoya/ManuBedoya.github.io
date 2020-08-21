abbbfbd2022f9f0dbcd473d2f61e6a6611b30f8c
Manuel <manuelfernandobedoya@gmail.com>
2020-08-20 21:12:55 -0500

    se agregaron comentarios

--- behavor.js
+++ behavor.js
@@ -1,41 +1,41 @@
+//Variables globales las cuales representa los 2 numeros que se van a operar
+
 let firstNumber = 0;
 let secondNumber = 0;
-
+//Función Sumar
 function sumar() {
-  firstNumber = document.getElementById("number1").value;
-  secondNumber = document.getElementById("number2").value;
+  asignarValores();
   if (verificarNumerosVacios()) {
     return;
   }
   document.getElementById("resultado").innerHTML =
     parseFloat(firstNumber) + parseFloat(secondNumber);
 }
-
+//Función Restar
 function restar() {
-  firstNumber = document.getElementById("number1").value;
-  secondNumber = document.getElementById("number2").value;
+  asignarValores();
   if (verificarNumerosVacios()) {
     return;
   }
   document.getElementById("resultado").innerHTML =
     parseFloat(firstNumber) - parseFloat(secondNumber);
 }
+//Función multiplicar
 function multiplicar() {
-  firstNumber = document.getElementById("number1").value;
-  secondNumber = document.getElementById("number2").value;
+  asignarValores();
   if (verificarNumerosVacios()) {
     return;
   }
   document.getElementById("resultado").innerHTML =
     parseFloat(firstNumber) * parseFloat(secondNumber);
 }
-
+//Función dividir
 function dividir() {
-  firstNumber = document.getElementById("number1").value;
-  secondNumber = document.getElementById("number2").value;
+  asignarValores();
   if (verificarNumerosVacios()) {
     return;
   }
+  //Una división no puede ser entre 0 asi que se valida antes de hacer la operación
   if (secondNumber == 0) {
     alert("No se puede dividir entre 0");
     return;
@@ -43,7 +43,7 @@ function dividir() {
   document.getElementById("resultado").innerHTML =
     parseFloat(firstNumber) / parseFloat(secondNumber);
 }
-
+//Función que valida si tanto el campo del valor 1 o el valor 2 estan vacios , en caso de estarlos retorna true de lo contrario false;
 function verificarNumerosVacios() {
   if (firstNumber == "" || secondNumber == "") {
     alert("Debe ingresar 2 numeros");
@@ -51,4 +51,9 @@ function verificarNumerosVacios() {
   } else {
     return false;
   }
+}
+//Función que coge los valores de los imputs del html y los guarda en sus respectivas variables
+function asignarValores() {
+  firstNumber = document.getElementById("number1").value;
+  secondNumber = document.getElementById("number2").value;
 }
